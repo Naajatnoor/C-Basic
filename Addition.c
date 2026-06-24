30ba60e24fe267a40886188db142ff3b9dc069ee
Naajatnoor <295597711+Naajatnoor@users.noreply.github.com>
2026-06-24 21:15:36 +0600

    initial commit

---
+++ Addition.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num1=1, num2=2, sum;
+
+    // Perform addition
+    sum = num1 + num2;
+
+    // Output the result
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
